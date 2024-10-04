fe39b52c4291c6d0e43b40bcf1026fac2a6e94a5
Itzli Molina <itzli2000@msn.com>
2024-10-03 19:37:21 -0600

    fix(Release app): use generate-changelog directly with npx

--- packages/release-cli/src/services/changelogService.ts
+++ packages/release-cli/src/services/changelogService.ts
@@ -4,7 +4,7 @@ import logger from '../utils/logger';
 class ChangelogService {
   public generateChangelog(releaseType: string): void {
     logger.info('Generando changelog...');
-    const command = `npx changelog ${releaseType} -x other,build,ci,chore,refactor,revert`;
+    const command = `npx generate-changelog ${releaseType} -x other,build,ci,chore,refactor,revert`;
     if (shell.exec(command).code !== 0) {
       throw new Error('No se pudo generar el changelog.');
     }
